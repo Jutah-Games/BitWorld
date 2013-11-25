2b4fe3aee4326ceea1810101f82d529f83d07dfc
Dennis <dennis.jutemark@telia.com>
2013-11-25 20:54:48 +0100

    Parse SDK
    Removed the old project and created a new one with Parse SDK and
    Facebook SDK

---
+++ UnityProject/Assets/_Game/Scripts/TestScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Parse;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
