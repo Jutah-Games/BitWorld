d6edeba2e2acbd09d538f78ddf7c63ba336012fc
Dennis <dennis.jutemark@telia.com>
2013-12-05 13:35:48 +0100

    Removed Unnecessary Files

--- UnityProject 2.0/Assets/UI.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class UI : MonoBehaviour {
-	// The player User Interface
-	void OnGUI () {
-
-	}
-}
---
+++ UnityProject/Assets/_Game/Scripts/PlayerScripts/PlayerMovement-Dennis.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using Parse;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+#region Variables
+	public float movementSpeed = 2f;
+	public float jumpHeight = 2f;
+
+	private Transform groundCheck;
+
+	public float maxMovementSpeed = 10f;
+
+	private bool grounded = false;
+	private bool jump = false;
+
+#endregion
+
+	void Awake () {
+											// Import Animations Here!!
+		groundCheck = transform.Find("groundCheck");
+	}
+
+	void Update() {
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+
+		if(Input.GetButtonDown("Jump") && grounded)
+			jump = true;
+
+		if(Application.isEditor)
+			if(Input.GetKeyDown(KeyCode.F5)) {
+				transform.position = new Vector3(0, 4, 0);
+				transform.rotation = new Quaternion(0, 0, 0, 0);
+			}
+	}
+
+	void FixedUpdate() {
+		playerMovement();
+
+		if (jump) {
+			rigidbody2D.AddForce(new Vector2(0f, jumpHeight * 100f));
+
+			jump = false;
+		}
+
+	}
+
+	void playerMovement() {
+		float h = Input.GetAxis ("Horizontal");
+
+		if ((rigidbody2D.velocity.x * h < maxMovementSpeed) && grounded) 
+			rigidbody2D.AddForce (Vector2.right * h * 2 * movementSpeed);
+	
+		if (Input.GetKey (KeyCode.A) && grounded)
+			rigidbody2D.AddForce (Vector2.right * movementSpeed);
+	}
+
+	void OnGUI() {
+//		GUI.Box (new Rect (25, 25, 50, 25), "" + Input.GetAxis ("Horizontal"));
+	}
+
+}
